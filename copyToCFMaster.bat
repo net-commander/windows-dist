10f88d663f1379f3aa6575d7fcccc75c6f863d40
xblox <asd28071977>
2017-09-05 07:20:02 +0200

    =latest

--- copyToCFMaster.bat
+++
@@ -1,7 +0,0 @@
-for /f %%i in ("%0") do set curpath=%%~dpi
-cd /d %curpath%
-
-copy y:\Code\client\src\xcf\dojo\xcf.js .\Code\client\src\xcf\dojo\xcf.js
-copy y:\server\nodejs\build\server\nxappmain\serverbuild.js.uncompressed.js .\server\windows\nxappmain\serverbuild.js
-copy Y:\Code\client\src\lib\xapp\build\main_build.js .\Code\client\src\lib\xapp\build\main_build.js
-xcopy /y /s y:\server\nodejs\dist\all\_build server\windows\_build
