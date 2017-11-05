faba99c5485befdd19ca4ed744f6cd2d77e7458f
xblox <asd28071977>
2017-11-05 10:07:55 +0100

    =save only

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 setlocal
 cd /d %~dp0
 cd server\windows
-server.exe noob --type=OFFLINE_RELEASE --release=true --root="..\.." %*
+server.exe noob --home=true --type=OFFLINE_RELEASE --release=true --root="..\.." %*
